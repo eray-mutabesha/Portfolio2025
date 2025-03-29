136ebf0b2b775890f531148a1c59477a926fd185
eray-mutabesha <eraymutabesha13@gmail.com>
2025-03-29 13:40:52 +0200

    about component

---
+++ resources/views/components/abouts.blade.php
@@ -0,0 +1,174 @@
+
+       
+      <div class="container">
+        <div class="row mb-lg-120 mb-56">
+          <div class="col-lg-5 mb-5 mb-lg-0">
+            <!-- Image Profile -->
+            <div class="img-wrap-s2">
+              <div class="img-wrap-s2-inner">
+                <div class="img-wrap-s2-circle wow fadeInRight"></div>
+                <img src="assets/images/eray006.png" alt="" class="d-inline-block wow fadeScaleOutIn"
+                  data-wow-delay="400ms">
+              </div>
+            </div>
+            <!-- /Image Profile -->
+
+            <div class="w-100 mt-32">
+              <div class="mb-32 wow fadeInUp">
+                <h3 class="fs-5xl mb-0">Eray mutabesha</h3>
+                <div class="text-gray-2 fs-xl">Software developer</div>
+              </div>
+              <!-- Biography -->
+              <div class="mb-16 wow fadeInUp" data-wow-delay="200ms">
+                <h4>About me</h4>
+                <p class="text-gray-2">👋 Hi there! I'm Eray mutabesha AKA mr 404, a passionate software engineer specializing in JavaScript ,React JS, Node JS and Laravel. With a keen eye for clean and efficient code, I thrive on building dynamic and intuitive web applications that deliver exceptional user experiences, adept at crafting scalable solutions and leveraging cutting-edge technologies to solve complex problems.</p>
+              </div>
+              <!-- /Biography -->
+              <!-- Contact Information -->
+              <div class="wow fadeInUp" data-wow-delay="400ms">
+                <div class="d-flex justify-content-between mb-2">
+                </div>
+                <div class="d-flex justify-content-between mb-2">
+                  <div class="fw-600">Email</div>
+                  <div class="text-end text-gray-2 fs-lg">eraymutabesha4@gmail.com</div>
+                </div>
+                <div class="d-flex justify-content-between">
+                  <div class="fw-600">Phone</div>
+                  <div class="text-end text-gray-2 fs-lg">+250 791434737</div>
+                </div>
+              </div>
+              <!-- /Contact Information -->
+            </div>
+          </div>
+
+          <div class="col-lg-6 offset-lg-1">
+            <!-- Education & Experiences -->
+            <div>
+              <h3 class="section-title-s4 mb-32 mb-lg-48 wow fade-right" data-splitting="chars">
+                Published Works
+              </h3>
+              <div class="position-relative">
+                <!-- <div class="bg-blurred-red-1"></div> -->
+                <div class="bg-blurred-red"></div>
+                <div class="bg-blurred-purple"></div>
+                <!-- Experience Card -->
+                <div class="experience-card-s2 mb-24 wow fadeInUp">
+                  <div class="experience-card-s2__org"><a href="https://lagracedejnk.v2.nguvutech.com/">La Grâce de JNK Restaurant </a></div>
+                  <div class="experience-card-s2__duration">2025</div>
+                  <div class="experience-card-s2__title">Restaurant management system</div>
+                  <div class="experience-card-s2__desc">La Grace de JNK Restaurant is a comprehensive restaurant management system designed to optimize operations and enhance customer experience. It includes features such as order management, reservation handling, inventory tracking, and a seamless payment system. Built with a user-friendly interface and robust backend architecture, it ensures efficiency for both staff and customers.
+                  </div>
+                </div>
+                <!-- /Experience Card -->
+
+
+                  <!-- Experience Card -->
+                  <div class="experience-card-s2 mb-24 wow fadeInUp">
+                  <div class="experience-card-s2__org"><a href="https://sms.nguvutech.com/">S.M.S</a></div>
+                  <!-- <div class="experience-card-s2__duration">2024</div> -->
+                  <div class="experience-card-s2__title">School management system</div>
+                  <div class="experience-card-s2__desc">
+                  Built with our dev team at #NGUVU TECH
+                 I had the opportunity to collaborate on the development of a complete management system for a school, as part of a group project. This project aimed to facilitate the management of the various administrative and academic operations of a school. We worked as a team to design an efficient, modern and scalable solution that meets the specific needs of teachers, administrators, students and parents.
+                  </div>
+                </div>
+                <!-- /Experience Card -->
+
+              </div>
+
+            </div>
+            <!-- /Education & Experiences -->
+          </div>
+        </div>
+        <!-- My Skills -->
+        <div class="row">
+          <div class="col-12">
+            <h3 class="section-title-s5 stroke-text-bolder text-center mb-48 mb-xl-56 wow fade-right"
+              data-splitting="chars">
+              My Expertise</h3>
+          </div>
+          <div class="col-lg-6 position-relative mb-40 mb-lg-0">
+            <div class="bg-blurred-red"></div>
+            <div class="bg-blurred-purple"></div>
+            <div class="row mt-lg-48">
+              <div class="col-12 col-sm-6 mb-32">
+                <div class="counter-card-s2 wow-num fadeInRight" data-wow-delay="1s">
+                  <h4 class="counter-card-s2__digit"><span class="odometer-number" data-number="4"></span>+</h4>
+                  <p class="text-gray-3">Years of Experience</p>
+                </div>
+              </div>
+              <div class="col-12 col-sm-6 mb-32">
+                <div class="counter-card-s2 wow-num fadeInRight" data-wow-delay="1.2s">
+                  <h4 class="counter-card-s2__digit"><span class="odometer-number" data-number="10"></span>+</h4>
+                  <p class="text-gray-3">Websites Built</p>
+                </div>
+              </div>
+              <div class="col-12 col-sm-6 mb-32 mb-lg-0">
+                <div class="counter-card-s2 wow-num fadeInRight" data-wow-delay="1s">
+                  <h4 class="counter-card-s2__digit"><span class="odometer-number" data-number="20"></span>+</h4>
+                  <p class="text-gray-3">Completed project</p>
+                </div>
+              </div>
+              <div class="col-12 col-sm-6">
+                <div class="counter-card-s2 wow-num fadeInRight" data-wow-delay="1.2s">
+                  <h4 class="counter-card-s2__digit"><span class="odometer-number" data-number="20"></span>+</h4>
+                  <p class="text-gray-3">Published Works</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5 offset-lg-1 position-relative">
+            <!-- Skill Progress -->
+            <div class="skill-progress-container">
+              <!-- Skill Progress Item -->
+              <div class="skill-progress-item wow fadeInUp">
+                <h6 class="title">PHP/Laravel</h6>
+                <div class="progress" role="progressbar" aria-label="Progress label" aria-valuenow="85"
+                  aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar wow slideInLeft" data-wow-delay=" .1s" style="width: 90%"><span>90%</span>
+                  </div>
+                </div>
+              </div>
+              <!-- /Skill Progress Item -->
+
+              <!-- Skill Progress Item -->
+              <div class="skill-progress-item wow fadeInUp" data-wow-delay="200ms">
+                <h6 class="title">JavaScript </h6>
+                <div class="progress" role="progressbar" aria-label="Progress label" aria-valuenow="90"
+                  aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar wow slideInLeft" data-wow-delay=" .3s" style="width: 85%"><span>85%</span>
+                  </div>
+                </div>
+              </div>
+              <!-- /Skill Progress Item -->
+
+
+              <!-- Skill Progress Item -->
+              <div class="skill-progress-item wow fadeInUp" data-wow-delay="400ms">
+                <h6 class="title">Node JS</h6>
+                <div class="progress" role="progressbar" aria-label="Progress label" aria-valuenow="85"
+                  aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar wow slideInLeft" data-wow-delay=" .5s" style="width: 85%"><span>85%</span>
+                  </div>
+                </div>
+              </div>
+              <!-- /Skill Progress Item -->
+
+              <!-- Skill Progress Item -->
+              <div class="skill-progress-item wow fadeInUp" data-wow-delay="600ms">
+                <h6 class="title">React JS</h6>
+                <div class="progress" role="progressbar" aria-label="Progress label" aria-valuenow="80"
+                  aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar wow slideInLeft" data-wow-delay=" .7s" style="width: 85%"><span>85%</span>
+                  </div>
+                </div>
+              </div>
+              <!-- /Skill Progress Item -->
+
+            </div>
+            <!-- /Skill Progress -->
+          </div>
+        </div>
+        <!-- /My Skills -->
+      </div>
+
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <meta name="description" content="Name of your web site">
+  <meta name="author" content="NusaTheme">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <title>Eray | Personal Portfolio</title>
+  <!-- Favicon -->
+  <link rel="icon" href="assets/images/favicon.png" type="image/png">
+  <!-- /Favicon -->
+
+  <!-- STYLES -->
+  <link rel="preconnect" href="https://fonts.googleapis.com/">
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&amp;display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-icons.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/swiper-bundle.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/splitting.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <!-- /STYLES -->
+</head>
+
+<body class="dark">
+  <header class="site-header sticky wow fadeInDown">
+    <div class="container">
+      <div class="site-header-container">
+        <!-- Logo -->
+        <div class="site-header-logo">
+          <a href="#" class="logo">
+            <img src="assets/images/logo.png" alt="Your Logo" width="120" height="34">
+            <span class="visually-hidden">Logo</span>
+          </a>
+        </div>
+        <!-- /Logo -->
+
+        <!-- Menu -->
+        <div class="mobile-menu d-lg-none">
+          <button class="js-menu-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <nav class="site-menu text-uppercase">
+          <ul>
+            <li><a href="#home" class="one-page-link active">Home</a></li>
+            <li><a href="#service" class="one-page-link">Services</a></li>
+            <li><a href="#about" class="one-page-link">About Me</a></li>
+            <li><a href="#portfolio" class="one-page-link">Portfolio</a></li>
+            <li class="has-submenu">
+              <a href="#">Blog <i class="fa fa-chevron-down"></i></a>
+              <ul class="submenu">
+                <li><a href="blog-list.html">Blog List</a></li>
+                <li><a href="blog-detail.html">Blog Detail</a></li>
+              </ul>
+            </li>
+            <li><a href="#contact" class="one-page-link">Contact</a></li>
+          </ul>
+        </nav>
+        <!-- /Menu -->
+
+        <!-- Button Hire Me -->
+        <div class="d-none d-xl-flex site-menu-right">
+          <a href="#contact" class="btn btn-md one-page-link">
+            Hire Me <i class="fa fa-chevron-right ms-1 fs-sm"></i>
+          </a>
+        </div>
+        <!-- Button Hire Me -->
+      </div>
+    </div>
+
+  </header>
+
+  <!-- all content -->
+  {{$slot}}
+  <!-- all content -->
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <!-- Footer Copyright -->
+        <div class="col-lg-5 mb-4 mb-lg-0 text-center text-lg-start">
+          <span class="text-gray-2 d-inline-block wow fadeInLeft" data-wow-offset="20">©
+            Eray mutabesha 2025 | All Rights
+            Reserved</span>
+        </div>
+        <!-- /Footer Copyright -->
+
+        <!-- Social Media -->
+        <div class="col-lg-7">
+          <div class="social-media wow fadeInLeft" data-wow-offset="20">
+            <a href="https://www.facebook.com/dieumercie.mutabesha?locale=fr_FR">
+              <span class="visually-hidden">Facebook</span>
+              <i class="bi-facebook"></i>
+            </a>
+            <a href="https://x.com/ErayMutabesha">
+              <span class="visually-hidden">Twitter</span>
+              <i class="bi-twitter-x"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/eray-mutabesha-186240256/">
+              <span class="visually-hidden">Linkedin</span>
+              <i class="bi-linkedin"></i>
+            </a>
+            <a href="https://www.instagram.com/its.eray_/">
+              <span class="visually-hidden">Instagram</span>
+              <i class="bi-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <!-- /Social Media -->
+
+      </div>
+
+    </div>
+  </footer>
+  <!-- /Footer -->
+
+  <!-- Preloader -->
+  <div class="preloader">
+    <div class="ripple">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <!-- /Preloader -->
+
+  <!-- CURSOR -->
+  <div class="mouse-cursor cursor-outer"></div>
+  <div class="mouse-cursor cursor-inner"></div>
+  <!-- /CURSOR -->
+
+  <!-- Back to top -->
+  <div class="back-to-top-wrapper">
+    <a href="#Home" class="back-to-top">
+      <span class="visually-hidden">Back to top</span>
+      <i class="bi bi-arrow-up-short"></i>
+      <div class="progress-wrap active-progress">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+      </div>
+    </a>
+  </div>
+  <!-- /Back to top -->
+
+  <!-- SCRIPTS -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>
+  <!-- /SCRIPTS -->
+</body>
+
+
+</html>
